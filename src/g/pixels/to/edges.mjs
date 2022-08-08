39e8aedb8211596694d6e6238ab3c44d2fdb725a
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:35:34 -0400

    main noop progress

--- src/g/pixels/to/edges.mjs
+++ src/g/pixels/to/edges.mjs
@@ -2,13 +2,11 @@ import {list_index_last} from "./../../../../node_modules/mykro/src/list/index/l
 import {m_js_for_each} from "./../../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
+import { list_index_first_or_last_is } from "../../../list/index/first/or/last/is.mjs";
 export async function g_pixels_to_edges(pixels) {
   await m_js_arguments_assert(list_is)(arguments);
   await m_js_for_each(pixels, async (row, row_index) => {
-    if (row_index === 0) {
-      return;
-    }
-    if (row_index === await list_index_last(pixels)) {
+    if (await list_index_first_or_last_is(pixels, row_index)) {
       return;
     }
     await m_js_for_each(row, async (col, col_index) => {
