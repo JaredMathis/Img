710527d4738f996b16445fe22dc112bea02da2cd
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:33:21 -0400

    main file_js_auto g_pixels_to_edges

--- src/g/pixels/to/edges.mjs
+++ src/g/pixels/to/edges.mjs
@@ -1,11 +1,21 @@
+import {list_index_last} from "./../../../../node_modules/mykro/src/list/index/last.mjs";
 import {m_js_for_each} from "./../../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_pixels_to_edges(pixels) {
   await m_js_arguments_assert(list_is)(arguments);
   await m_js_for_each(pixels, async (row, row_index) => {
-    await m_js_for_each(row, async (col, col_index)  => {
-      
+    if (row_index === 0) {
+      return;
+    }
+    if (row_index === await list_index_last(pixels)) {
+      return;
+    }
+    await m_js_for_each(row, async (col, col_index) => {
+      if (col !== 1) {
+        return;
+      }
+      let up = pixels[row_index - 1];
     });
   });
 }
