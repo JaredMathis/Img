6e01cf79e1d87c5066e2acbf5faa1e3c83c4119e
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:27:28 -0400

    main noop progress

--- src/g/pixels/to/edges.mjs
+++ src/g/pixels/to/edges.mjs
@@ -1,4 +1,4 @@
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function g_pixels_to_edges() {
-  await m_js_arguments_assert()(arguments);
+export async function g_pixels_to_edges(pixels) {
+  await m_js_arguments_assert(list_is)(arguments);
 }
