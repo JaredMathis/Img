c49e5c5a1bf66c63a89bfe87541d6af1220ed514
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:30:42 -0400

    main file_js_auto g_pixels_to_edges

--- src/g/pixels/to/edges.mjs
+++ src/g/pixels/to/edges.mjs
@@ -1,5 +1,9 @@
+import {m_js_for_each} from "./../../../../node_modules/mykro/src/m/js/for/each.mjs";
 import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_pixels_to_edges(pixels) {
   await m_js_arguments_assert(list_is)(arguments);
+  await m_js_for_each(pixels, async row => {
+    await m_js_for_each(row, async col => {});
+  });
 }
