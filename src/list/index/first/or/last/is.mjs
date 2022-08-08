f61967e069658318140117eac531ba914b112867
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:35:03 -0400

    main file_js_auto list_index_first_or_last_is

--- src/list/index/first/or/last/is.mjs
+++ src/list/index/first/or/last/is.mjs
@@ -1,4 +1,14 @@
+import {list_index_last} from "./../../../../../../node_modules/mykro/src/list/index/last.mjs";
+import {m_js_number_is} from "./../../../../../../node_modules/mykro/src/m/js/number/is.mjs";
+import {list_is} from "./../../../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
-export async function list_index_first_or_last_is() {
-  await m_js_arguments_assert()(arguments);
+export async function list_index_first_or_last_is(list, index) {
+  await m_js_arguments_assert(list_is, m_js_number_is)(arguments);
+  if (index === 0) {
+    return true;
+  }
+  if (index === await list_index_last(list)) {
+    return true;
+  }
+  return false;
 }
