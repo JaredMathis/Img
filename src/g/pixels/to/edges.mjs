6a33d0fb9b3b4270c7881077416336630d017989
JaredMathis <jared.mathis@native.consulting>
2022-08-07 23:31:33 -0400

    main noop progress

--- src/g/pixels/to/edges.mjs
+++ src/g/pixels/to/edges.mjs
@@ -3,7 +3,9 @@ import {list_is} from "./../../../../node_modules/mykro/src/list/is.mjs";
 import {m_js_arguments_assert} from "./../../../../node_modules/mykro/src/m/js/arguments/assert.mjs";
 export async function g_pixels_to_edges(pixels) {
   await m_js_arguments_assert(list_is)(arguments);
-  await m_js_for_each(pixels, async row => {
-    await m_js_for_each(row, async col => {});
+  await m_js_for_each(pixels, async (row, row_index) => {
+    await m_js_for_each(row, async (col, col_index)  => {
+      
+    });
   });
 }
