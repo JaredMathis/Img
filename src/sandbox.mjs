38decf41e58618d2bca24c6cf36ac95faa59f466
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:27:55 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,17 +1,8 @@
+import {m_js_for_each} from "./../node_modules/mykro/src/m/js/for/each.mjs";
 export async function sandbox() {
-  let simple = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0],
-  ];
-
-  let expected = [
-    [[1, 1], [1, 2]]
-    , [[1, 1], [2, 1]]
-    , [[2, 1], [2, 2]]
-    , [[2, 1], [2, 2]]
-  ]
+  let simple = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+  let expected = [[[1, 1], [1, 2]], [[1, 1], [2, 1]], [[2, 1], [2, 2]], [[2, 1], [2, 2]]];
 }
-function to_rectangle() {
-
+async function to_segments(grid) {
+  await m_js_for_each(grid, row => {});
 }
