ab9ac7a764fad512cc3c615f9eb419ac66deb1ed
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:33:28 -0400

    main sandbox sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -7,11 +7,11 @@ export async function sandbox() {
 }
 async function to_segments(grid) {
   await m_js_for_each(grid, async (row, row_index) => {
-    if (await list_index_first_or_last_is(list, row_index)) {
+    if (await list_index_first_or_last_is(grid, row_index)) {
       return;
     }
     await m_js_for_each(row, async (col, col_index) => {
-      if (await list_index_first_or_last_is(list, row_index)) {
+      if (await list_index_first_or_last_is(row, col_index)) {
         return;
       }
       let value = grid[row_index][col_index];
