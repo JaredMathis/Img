4e190e2d8b9bdd6103eff894dd83e6fc7373eedb
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:44:13 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,3 +1,4 @@
+import {list_add} from "./../node_modules/mykro/src/list/add.mjs";
 import {list_index_first_or_last_is} from "./list/index/first/or/last/is.mjs";
 import {m_js_for_each} from "./../node_modules/mykro/src/m/js/for/each.mjs";
 export async function sandbox() {
@@ -20,14 +21,27 @@ async function to_segments(grid) {
         return;
       }
       let neighbors = {
-        up: {row:row_index-1,col:col_index},
-        down: {row:row_index+1,col:col_index},
-        left: {row:row_index,col:col_index-1},
-        right: {row:row_index,col:col_index+1},
+        up: {
+          row: row_index - 1,
+          col: col_index
+        },
+        down: {
+          row: row_index + 1,
+          col: col_index
+        },
+        left: {
+          row: row_index,
+          col: col_index - 1
+        },
+        right: {
+          row: row_index,
+          col: col_index + 1
+        }
       };
       for (let neighbor in neighbors) {
-        if (neighbors[neighbor] === 0) {
-          await list_add(result, )
+        let n = neighbors[neighbor];
+        if (grid[n.row][n.col] === 0) {
+          await list_add(result, midpoint(n.col, n.row, row_index, col_index));
         }
       }
       console.log({
@@ -38,4 +52,7 @@ async function to_segments(grid) {
       });
     });
   });
+}
+function midpoint(x1, y1, x2, y2) {
+  return [(y1 + y2) / 2, (x1 + x2) / 2];
 }
