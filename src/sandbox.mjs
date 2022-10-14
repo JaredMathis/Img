6d6a04cbb717d744482fa46caafba259d0d18bab
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:26:21 -0400

    main noop progress

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -2,7 +2,16 @@ export async function sandbox() {
   let simple = [
     [0, 0, 0],
     [0, 1, 0],
-    [0, 1, 0],
     [0, 0, 0],
+  ];
+
+  let expected = [
+    [[1, 1], [1, 2]]
+    , [[1, 1], [2, 1]]
+    , [[2, 1], [2, 2]]
+    , [[2, 1], [2, 2]]
   ]
+}
+function to_rectangle() {
+
 }
