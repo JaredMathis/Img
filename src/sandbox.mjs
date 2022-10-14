345352b2c71125c940de5926818fde032f7ffbcf
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:32:03 -0400

    main file_js_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,3 +1,4 @@
+import {list_index_first_or_last_is} from "./list/index/first/or/last/is.mjs";
 import {m_js_for_each} from "./../node_modules/mykro/src/m/js/for/each.mjs";
 export async function sandbox() {
   let simple = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
@@ -6,7 +7,13 @@ export async function sandbox() {
 }
 async function to_segments(grid) {
   await m_js_for_each(grid, async (row, row_index) => {
+    if (await list_index_first_or_last_is(list, row_index)) {
+      return;
+    }
     await m_js_for_each(row, async (col, col_index) => {
+      if (await list_index_first_or_last_is(list, row_index)) {
+        return;
+      }
       let value = grid[row_index][col_index];
       console.log({
         row_index,
