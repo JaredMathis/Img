a2b227b6764343a06cff0a4cbc584987dc3fb550
JaredMathis <jared.mathis@native.consulting>
2022-10-13 20:38:25 -0400

    main sandbox sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -6,6 +6,7 @@ export async function sandbox() {
   let actual = await to_segments(simple);
 }
 async function to_segments(grid) {
+  let result = [];
   await m_js_for_each(grid, async (row, row_index) => {
     if (await list_index_first_or_last_is(grid, row_index)) {
       return;
@@ -14,13 +15,21 @@ async function to_segments(grid) {
       if (await list_index_first_or_last_is(row, col_index)) {
         return;
       }
-      let neighbors = {
-        up: grid[row_index-1][col_index],
-        down: grid[row_index+1][col_index],
-        left: grid[row_index][col_index-1],
-        right: grid[row_index][col_index+1],
-      }
       let value = grid[row_index][col_index];
+      if (value !== 1) {
+        return;
+      }
+      let neighbors = {
+        up: {row:row_index-1,col:col_index},
+        down: {row:row_index+1,col:col_index},
+        left: {row:row_index,col:col_index-1},
+        right: {row:row_index,col:col_index+1},
+      };
+      for (let neighbor in neighbors) {
+        if (neighbors[neighbor] === 0) {
+          await list_add(result, )
+        }
+      }
       console.log({
         row_index,
         col_index,
